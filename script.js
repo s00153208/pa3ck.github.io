1de0d6291b0d2e115eba346787b15946a01cb060
s00153208 <s00153208@mail.itsligo.ie>
2016-10-13 13:54:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 
-var entries = [
- 
-  { title = "Some title", date = "13/10/2016", desc = "some desc...." },   
-  { title = "Some title", date = "13/10/2016", desc = "some desc...." },  
-  
-];
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.blogs = [
+        {title:"Title", date:"13/10/2016", cont:"Some description for this blog"},
+        {title:"Blog", date:"07/10/2016", cont:"Setup GitHub page for blog"},
+    ];
+});
+
+app.directive('blogEntry', function() {
+    return {
+        restrict: 'E', // Element directive
+        scope: { blog: '=blogEntry' },
+        template: 
+        `<div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"> {{ blog.title }} <span class="pull-right"> {{ blog.date }}</span></h3>
+            </div>
+            <div class="panel-body">
+                {{ blog.cont }}
+            </div>
+        </div>`
+    };
+});
